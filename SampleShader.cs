0ebf73e29d75ad1f8cf08fbfd82e2afd5f010a47
Dmitri <56200546+Syritx@users.noreply.github.com>
2021-01-10 11:57:26 -0500

    added source

---
+++ SampleShader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SampleShader : MonoBehaviour
+{
+    [SerializeField] ComputeShader shader;
+    public RenderTexture result;
+
+    [Range(-1,1)] public float brightness = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        int kernel = shader.FindKernel("CSMain");
+
+        result = new RenderTexture(512, 512, 24);
+        result.enableRandomWrite = true;
+        result.Create();
+
+        shader.SetFloat("resolution", result.width);
+        shader.SetFloat("brightness", brightness);
+        shader.SetTexture(kernel, "Result", result);
+        shader.Dispatch(kernel, 512 / 8, 512 / 8, 1);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (result == null)
+        {
+            result = new RenderTexture(512, 512, 24);
+            result.enableRandomWrite = true;
+            result.Create();
+        }
+
+        shader.SetTexture(shader.FindKernel("CSMain"), "Result", result);
+        shader.SetFloat("brightness", brightness);
+        shader.Dispatch(shader.FindKernel("CSMain"), 512 / 8, 512 / 8, 1);
+
+        Graphics.Blit(result, destination);
+    }
+}
